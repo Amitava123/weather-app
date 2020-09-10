b39a603962fa1705025ca6be24e3a49fc641bec1
Amitava Mitra <mitraamitava7@gmail.com>
2020-09-10 09:18:41 +0530

    Use Celsius in forcast
    Signed-off-by: Amitava Mitra <mitraamitava7@gmail.com>

--- src/utils/getForcast.js
+++ src/utils/getForcast.js
@@ -1,7 +1,7 @@
 const request = require('request');
 
 const getForcast = (long, lat, callback) => {
-    var url = 'https://api.openweathermap.org/data/2.5/onecall?lat='+ lat +'&lon='+ long +'&appid=c763e6c1b04053c90b76e81311131680';
+    var url = 'https://api.openweathermap.org/data/2.5/onecall?lat='+ lat +'&lon='+ long +'&units=metric&appid=c763e6c1b04053c90b76e81311131680';
 
     request({url: url, json: true}, function (error, response, body) {
         if (error) {
